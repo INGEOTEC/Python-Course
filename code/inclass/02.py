58e41f1643623a241118828ecfdc84089cba1152
Mario Graff <mgraffg@ieee.org>
2021-09-01 13:13:25 -0400

    02

---
+++ code/inclass/02.py
@@ -0,0 +1,45 @@
+niña = 12
+
+# unicode can used in variables names
+print(niña)
+
+# an expression always returns something
+# None is something
+print(print('a'))
+
+# it wont work because it is a assignment
+print(a=3)
+
+# function that returns two numbers
+divmod(7, 2)
+
+# how to make a multiple assignment
+a, b = divmod(7, 2)
+
+a = "mario"
+y = "r"
+
+# operations on strings or sequences
+y in a
+
+y = "x"
+y in a
+
+b = "graff"
+# concatenation
+a + b
+
+# repetition
+a * 4
+
+# index
+a[2]
+
+# slide - subsequence
+a[2:4]
+
+# min element of the sequence a
+min(a)
+
+# index of an object in a sequence
+a.index('i')
