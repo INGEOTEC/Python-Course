7d0cf2175869f6331ea540a2dbbe357f635c7b5d
Mario Graff <mgraffg@ieee.org>
2021-09-03 12:24:21 -0400

    03

---
+++ code/inclass/03.py
@@ -0,0 +1,26 @@
+a = input("Please enter a number: ")
+print("The number is", a)
+# The number is 12
+
+a = 12
+print(a)
+a = 43
+print(a)
+
+a = "hi!"
+b = a
+a = "bye"
+print(a, b)
+
+x = 2
+x = (x + 3) * x
+print(x)
+
+a = input("Please enter a number 1: ")
+a = int(a)
+b = input("Please enter a number 2: ")
+b = int(b)
+c = a + b
+
+print("The sum of a + b is: ", c)
+
